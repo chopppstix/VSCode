62e6ad23164e01547ae27039fef74035208802b7
chopppstix <serpinskiy.roma@mail.ru>
2022-01-14 14:00:17 +0300

    Dz1
    homework

---
+++ DZ 1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n, i, p;
+	int temp;
+	cout << "Vvedite n";
+	cin >> n;
+	int mas[n];
+	for (i = 0; i < n; i++)
+		mas[i] =rand() %100-50;
+	for (int i = 0; i < n; i++){
+		cout <<mas[i]<<"";
+		cout<<endl;
+		}
+	for(int p=1; p<<n; p++)
+	for(i=0; i<n-1; i++)
+	if(mas[i] < mas[i+1]){
+		temp=mas[i];
+		mas[i]=mas[i+1];
+		mas[i+1]=temp;}
+		for(i=0;i<n;i++)
+		cout<<" "<<mas[i]<<"";
+		cout<<endl;
+}
